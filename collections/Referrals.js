50647b09fde9d71ab1bc9352ced8e68e331c95a5
c-artist <andilemkhizekhabazela@gmail.com>
2022-03-05 22:05:24 +0200

    referrals

--- collections/Referrals.js
+++ collections/Referrals.js
@@ -24,7 +24,7 @@ const Referrals = {
     slug: "referrals",
     admin: {
       useAsTitle: "referrals",
-      defaultColumns: ["referrals", "amount", "user", "lastPercent"],
+      defaultColumns: ["referrals", "user","amount", "lastPercent"],
     },
     access: {
       read: () => true,
